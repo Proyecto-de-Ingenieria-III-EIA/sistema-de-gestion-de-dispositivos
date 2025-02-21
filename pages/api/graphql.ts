5259df1b24295be8a67102a5e799f3513969ecce
Gersh0 <germinatorcofee@gmail.com>
2025-02-20 23:11:46 -0500

    feat(graphql): enhance user and role resolvers with authorization checks and context management

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,25 +1,37 @@
 import { resolvers, types } from '@/graphql';
+import { prisma } from '@/prisma';
+import { Context, AuthData } from '@/types';
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export const schema = makeExecutableSchema({ typeDefs: types, resolvers });
-
-interface Context {
-  db: PrismaClient;
-}
 
 const server = new ApolloServer<Context>({
   schema,
 });
 
 export default startServerAndCreateNextHandler(server, {
-  context: async () => {
+  context: async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.headers['session-token'];
+
+    const authData = (await prisma.$queryRaw`
+    select 
+    u.email,
+    r."name" as "role",
+    s.expires
+    from "Session" s
+        join "User" u
+            on s."userId" = u.id
+        join "Role" r
+            on u."roleId" = r.id
+    where s."sessionToken" = ${token}
+    `) as AuthData[];
+
     return {
       db: prisma,
+      authData: authData[0],
     };
   },
 });
