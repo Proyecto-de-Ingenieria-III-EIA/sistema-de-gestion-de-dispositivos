3d3db020b174226580a23f75bd1be121f73edb2a
Gersh0 <germinatorcofee@gmail.com>
2025-03-23 14:51:40 -0500

    cities & index

--- graphql/queries/city/types.ts
+++ graphql/queries/city/types.ts
@@ -3,7 +3,21 @@ import gql from 'graphql-tag';
 const cityTypes = gql`
   type City {
     id: ID!
-    name: String!      
+    name: String!
+  }
+
+  input CreateCityInput {
+    name: String!
+  }
+
+  extend type Query {
+    getCities: [City!]!
+    getCityById(id: ID!): City!
+    getCityByName(name: String!): City!
+  }
+
+  extend type Mutation {
+    createCity(input: CreateCityInput!): City!
   }
 `;
 
